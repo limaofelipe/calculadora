9a82081ef29b8d54edb025f9583c9cc4a3ed4420
limaofelipe <felipe08.tlima@gmail.com>
2020-04-19 18:45:10 -0300

    primeiro commit

---
+++ calc.js
@@ -0,0 +1,15 @@
+function populaTela(valor) {
+    document.getElementById("display").value += valor;
+}
+
+function limpaTela(valor) {
+    document.getElementById("display").value = valor;
+}
+
+function calcula() {
+    try {
+        limpaTela(eval(document.getElementById("display").value))
+    } catch (e) {
+        limpaTela("Erro!");
+    }
+}
